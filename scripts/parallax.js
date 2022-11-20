92bc2e1068b1c62c22c44f143d5e43296ba517e2
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2022-11-20 13:29:17 +0530

    Removed polygons

--- scripts/parallax.js
+++
@@ -1,6 +0,0 @@
-let polygons = document.getElementById("polygons");
-
-window.addEventListener("scroll", () => {
-    scrollValue = window.scrollY;
-    polygons.style.left = scrollValue * -0.3 + "px";
-});
